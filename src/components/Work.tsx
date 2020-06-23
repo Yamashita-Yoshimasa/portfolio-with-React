e87ab7c7ceefec61b626343047926cdba385f54d
Yamashita-Yoshimasa <yychbook040720@gmail.com>
2020-06-24 00:39:33 +0900

    ScrollToTop追加

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,8 +1,46 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable prefer-destructuring */
 import React, { FC } from 'react';
 import { Modal, Tab } from 'semantic-ui-react';
 import { WorkData } from './WorkList';
 
 const Work: FC<{}> = () => {
+  let PCTab: Element;
+  let SPTab: Element;
+  const GetTabItem = () => {
+    const DeviceTab = document.getElementsByClassName('item');
+    PCTab = DeviceTab[0];
+    SPTab = DeviceTab[1];
+  };
+
+  const ClassAddToDeactiveTab = () => {
+    if (SPTab.className === 'item') {
+      SPTab.classList.add('c-modal__deactive');
+    } else if (PCTab.className === 'item') {
+      PCTab.classList.add('c-modal__deactive');
+    }
+  };
+
+  const DeactiveTabCliclLisner = () => {
+    ClassAddToDeactiveTab();
+    const DeactiveTab = document.getElementsByClassName('c-modal__deactive')[0];
+    if (DeactiveTab) {
+      // eslint-disable-next-line @typescript-eslint/no-use-before-define
+      DeactiveTab.addEventListener('click', () => ScrollToTop());
+    }
+  };
+
+  const wait = (ms: number) =>
+    new Promise(resolve => setTimeout(() => resolve(), ms));
+
+  const ScrollToTop = async () => {
+    await wait(0);
+    GetTabItem();
+    DeactiveTabCliclLisner();
+    document.getElementsByClassName('c-modal__imageBox')[0].scrollTo(0, 0);
+  };
+
   return (
     <section id="works" className="l-works">
       <div className="p-works">
@@ -14,7 +52,7 @@ const Work: FC<{}> = () => {
               closeIcon
               key={item.id}
               trigger={
-                <li className="p-works__item">
+                <li className="p-works__item" onClick={() => ScrollToTop()}>
                   <p className="p-works__image-box">
                     <img
                       className="p-works__image"
