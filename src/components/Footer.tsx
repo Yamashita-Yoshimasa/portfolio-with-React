8614a7444d78e967aeb1e7106beda51595adb56e
Yamashita-Yoshimasa <yychbook040720@gmail.com>
2020-06-30 21:29:17 +0900

    Twitterリンク削除

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,13 +7,6 @@ const Footer: FC<{}> = () => (
       <address className="p-footer__address">
         <p className="p-footer__e-mail">Mail:yychbook040720@gmail.com</p>
       </address>
-      <div className="p-footer__fonts">
-        <a href="https://twitter.com/Tapi_tapirus">
-          <i className="fab fa-twitter-square p-footer__twitter" />
-          <p className="p-footer__twitter--text">My Twitter account</p>
-        </a>
-        <p className="p-skill__text" />
-      </div>
       <p className="p-footer__copy">
         <small>©︎ 2019 react-portfolio.yamabaku.work</small>
       </p>
